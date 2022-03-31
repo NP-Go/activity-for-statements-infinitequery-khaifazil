c1fe2545caf9fdb2cc6790233c21fc3859cc2717
khaifazil <khairal.fazil@gmail.com>
2022-03-31 20:35:18 +0800

    Completed activity

--- infiniteQuery.go
+++ infiniteQuery.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
 	//Insert your code here
+	//Hint if a:= ?? ; condition {  }
+	for {
+		var num int
+		fmt.Println("Enter number: ")
+		fmt.Scanln(&num)
+
+		if number := num; number%2 == 0 {
+			resultMessage := strconv.Itoa(number) + " is even"
+			fmt.Println(resultMessage)
+			if number < 9 && number > 0 {
+				fmt.Println("The number has only 1 digit")
+			} else {
+				fmt.Println("The number has more than 1 digit")
+			}
+		} else if number%2 == 1 {
+			resultMessage := strconv.Itoa(number) + " is odd"
+			fmt.Println(resultMessage)
+			if number < 9 && number > 0 {
+				fmt.Println("The number has only 1 digit")
+			} else {
+				fmt.Println("The number has more than 1 digit")
+			}
+		} else {
+			fmt.Println("This is not a number")
+		}
+	}
 }
